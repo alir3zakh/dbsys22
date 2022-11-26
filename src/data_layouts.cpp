c937fd651e91b7602cd9f1de4927ca5d3dd5bcf6
Alireza Kheradmand <kheradalireza@gmail.com>
2022-11-26 20:16:49 +0000

    Added hard-coded Data Layout

--- src/data_layouts.cpp
+++ src/data_layouts.cpp
@@ -9,7 +9,24 @@ using namespace m::storage;
 DataLayout MyNaiveRowLayoutFactory::make(std::vector<const Type*> types, std::size_t num_tuples) const
 {
     // TODO 1.2: implement computing a row layout
-    return DataLayout();
+    DataLayout DL;
+    auto &row = DL.add_inode(1, 224 * 8);
+    row.add_leaf(Type::Get_Integer(Type::TY_Vector, 4), 0, 0, 0);
+    row.add_leaf(Type::Get_Char(Type::TY_Vector, 10), 1, 32, 0);
+    row.add_leaf(Type::Get_Char(Type::TY_Vector, 32), 2, 128, 0);
+    row.add_leaf(Type::Get_Char(Type::TY_Vector, 20), 3, 384, 0);
+    row.add_leaf(Type::Get_Char(Type::TY_Vector, 80), 4, 544, 0);
+    row.add_leaf(Type::Get_Char(Type::TY_Vector, 32), 5, 1184, 0);
+    row.add_leaf(Type::Get_Integer(Type::TY_Vector, 8), 6, 1440, 0);
+    row.add_leaf(Type::Get_Char(Type::TY_Vector, 32), 7, 1502, 0);
+    row.add_leaf(Type::Get_Bitmap(Type::TY_Vector, 8), 8, 1760, 0);
+
+
+    for(auto e: types) {
+        std::cout << *e << ' ' << e->size() << ' ' << e->alignment() << ' ' << e->is_date() << std::endl;
+    }
+
+    return DL;
 }
 
 DataLayout MyOptimizedRowLayoutFactory::make(std::vector<const Type*> types, std::size_t num_tuples) const
