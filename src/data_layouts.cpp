f0260062a19f9a0129ac2f350f1fe3f2d7843374
Alireza Kheradmand <kheradalireza@gmail.com>
2022-11-27 00:24:52 +0000

    1.2 Naive Row Layout Completed

--- src/data_layouts.cpp
+++ src/data_layouts.cpp
@@ -10,24 +10,49 @@ DataLayout MyNaiveRowLayoutFactory::make(std::vector<const Type*> types, std::si
 {
     // TODO 1.2: implement computing a row layout
     DataLayout DL;
+
+    // Computing inode stride
     int INode_stride = 0;
-    for (auto& type : types)
-        INode_stride += ((type->size()-1) / 32 + 1) * 32;
+    uint64_t max_elem_alignemnt = 1;
 
-    auto &row = DL.add_inode(1, INode_stride + 32);
+    for (auto& type : types){
+        // updating max alignemnt seen until now
+        max_elem_alignemnt = std::max(max_elem_alignemnt, type->alignment());
+
+        // extending inode stride by the amount of current type (leaf in feature)
+        INode_stride += ((type->size()-1) / max_elem_alignemnt + 1) *                max_elem_alignemnt;
+    }
+
+    // Since minimum memory unit access is Byte, alignemnt could not be smaller
+    max_elem_alignemnt = std::max(max_elem_alignemnt, uint64_t(8));
+
+    // adding enough stride for NULL BITMAP
+    if(types.back()->size() % max_elem_alignemnt == 0)
+        INode_stride += max_elem_alignemnt;
+
+    // IF the stride is not a multiply of alignment, rounding it up to next multiply of alignment
+    if(INode_stride % max_elem_alignemnt)
+        INode_stride = ((INode_stride + types.size()) / max_elem_alignemnt + 1) * max_elem_alignemnt;
+
+    // Creading the inode
+    auto &row = DL.add_inode(1, INode_stride);
+
+    // Adding leafs to inode
     int cur_offset = 0, idx = 0;
+    uint64_t max_elem_alignemnt_till_now = 1;
 
-    for (auto& type : types)
-    {
+    for (auto& type : types){
         row.add_leaf(type, idx++, cur_offset, 0);
-        cur_offset += ((type->size()-1) / 32 + 1) * 32;
+
+        // updating max alignemnt seen until now
+        max_elem_alignemnt_till_now = std::max(max_elem_alignemnt_till_now, type->alignment());
+
+        // computing offset for next leaf
+        cur_offset += ((type->size()-1) / max_elem_alignemnt_till_now + 1) *                max_elem_alignemnt_till_now;
     }
     
+    // Bitmap leaf
     row.add_leaf(Type::Get_Bitmap(Type::TY_Vector, idx), idx, cur_offset, 0);
-
-    for(auto e: types) {
-        std::cout << *e << ' ' << e->size() << ' ' << e->alignment() << ' ' << std::endl;
-    }
 
     return DL;
 }
